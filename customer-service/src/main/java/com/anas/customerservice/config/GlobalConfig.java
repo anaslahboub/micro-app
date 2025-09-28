cd5dc9e93209e41043a27aea2e7b110a73eb9b09
AnasLahboub <anaslahbou11@gmail.com>
2025-09-28 23:35:47 +0100

    fixer erreur record

--- customer-service/src/main/java/com/anas/customerservice/config/GlobalConfig.java
+++ customer-service/src/main/java/com/anas/customerservice/config/GlobalConfig.java
@@ -1,10 +1,14 @@
 package com.anas.customerservice.config;
 
+import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "global.params")
 @RefreshScope
-public record GlobalConfig(int p1,int p2) {
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class GlobalConfig {
+    private int p1;
+    private int p2;
 }
